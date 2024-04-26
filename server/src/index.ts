35bb5527f48f388ee214b8eb03aef119b10051b7
Nayan Kunwar <nayankunwar678@gmail.com>
2024-04-26 17:56:21 +0530

    cloudinary setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,13 @@ import authRoutes from "./routes/auth";
 import cookie from "cookie-parser";
 import cookieParser from "cookie-parser";
 import path from "path";
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 //DB Connection
 connectToDb()
