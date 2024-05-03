6cc9dbd97524f1a8742c70d7781aec96464ae7e8
Nayan Kunwar <nayankunwar678@gmail.com>
2024-05-04 02:26:19 +0530

    HotelTypeFilter component

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -5,12 +5,14 @@ import { useState } from "react";
 import SearchResultsCard from "../components/SearchResultsCard";
 import Pagination from "../components/Pagination";
 import StarRatingFilter from "../components/StarRatingFilter";
+import HotelTypesFilter from "../components/HotelTypesFilter";
 const Search = () => {
     const search = useSearchContext();
     //console.log(search);
 
     const [page, setPage] = useState<number>(1);
     const [selectedStars, setSelectedStars] = useState<string[]>([]);
+    const [selectedHotelTypes, setSelectedHotelTypes] = useState<string[]>([]);
     const searchParams = {
         destination: search.destination,
         checkIn: search.checkIn.toISOString(),
@@ -19,6 +21,7 @@ const Search = () => {
         childCount: search.childCount.toString(),
         page: page.toString(),
         stars: selectedStars,
+        types: selectedHotelTypes
     };
     const { data: hotelData } = useQuery(["searchhotels", searchParams], () => apiClient.searchHotels(searchParams));
 
@@ -31,6 +34,16 @@ const Search = () => {
                 : prevStars.filter((star) => star !== starRating)
         );
     };
+
+    const handleHotelTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const hotelType = event.target.value;
+        setSelectedHotelTypes((prevHotelTypes) => { // {} it means we have to use return keyword.
+            return event.target.checked
+                ? [...prevHotelTypes, hotelType]
+                : prevHotelTypes.filter((hotel) => hotel != hotelType)
+
+        });
+    }
     return (
         <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
             <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
@@ -43,6 +56,10 @@ const Search = () => {
                     <StarRatingFilter
                         selectedStars={selectedStars}
                         onChange={handleStarsChange}
+                    />
+                    <HotelTypesFilter
+                        selectedHotelTypes={selectedHotelTypes}
+                        onChange={handleHotelTypeChange}
                     />
                 </div>
             </div>
