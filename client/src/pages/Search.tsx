a9912bd2c7e913636c5951c79cdb14e8135ca247
Nayan Kunwar <nayankunwar678@gmail.com>
2024-05-03 23:47:15 +0530

    create star rating filter component

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -4,11 +4,13 @@ import * as apiClient from "../api-client"
 import { useState } from "react";
 import SearchResultsCard from "../components/SearchResultsCard";
 import Pagination from "../components/Pagination";
+import StarRatingFilter from "../components/StarRatingFilter";
 const Search = () => {
     const search = useSearchContext();
     //console.log(search);
 
     const [page, setPage] = useState<number>(1);
+    const [selectedStars, setSelectedStars] = useState<string[]>([]);
     const searchParams = {
         destination: search.destination,
         checkIn: search.checkIn.toISOString(),
@@ -16,9 +18,19 @@ const Search = () => {
         adultCount: search.adultCount.toString(),
         childCount: search.childCount.toString(),
         page: page.toString(),
-
+        stars: selectedStars,
     };
     const { data: hotelData } = useQuery(["searchhotels", searchParams], () => apiClient.searchHotels(searchParams));
+
+    const handleStarsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const starRating = event.target.value;
+
+        setSelectedStars((prevStars) =>
+            event.target.checked
+                ? [...prevStars, starRating]
+                : prevStars.filter((star) => star !== starRating)
+        );
+    };
     return (
         <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
             <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
@@ -26,7 +38,12 @@ const Search = () => {
                     <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
                         Filter by:
                     </h3>
+
                     {/* TODO FILTERS */}
+                    <StarRatingFilter
+                        selectedStars={selectedStars}
+                        onChange={handleStarsChange}
+                    />
                 </div>
             </div>
             <div className="flex flex-col gap-5">
